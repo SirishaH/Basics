965fb49b965cdad665c998e67e5f13e3fd0e3de4
SirishaH <84061786+SirishaH@users.noreply.github.com>
2021-05-12 16:47:00 +0530

    Add files via upload

---
+++ Prime(All methods).cpp
@@ -0,0 +1,63 @@
+//PRIME NUMBER METHOD---- 1
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,count=0;
+	cout<<"Enter Number : ";
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	{
+	    if(n%i==0) 
+		{
+		   count++;
+		}
+	}
+		if(count==2)
+		   	cout<<"Given number is PRIME";
+		   else
+		   	cout<<"Given number is Not PRIME";			
+}
+
+//PRIME NUMBER METHOD----2
+ #include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,count=0;
+	cout<<"Enter number";
+	cin>>n;
+	for(int i=1;i<=n/2;i++) //i<=97/2 ; i<=48
+	{
+		if(n%i==0)
+		{                  // n=97
+			count++;
+		}            
+	}                
+	if(count==1)     
+	cout<<"Prime";   
+	else //            
+	cout<<"NOT PRIME"; 
+}
+
+//PRIME NUMBER METHOD----3
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,count=0;
+	cout<<"Enter number : ";
+	cin>>n;//100 
+	for(int i=1;i<=sqrt(n);i++)//sqrt(100)--->10
+	{
+		if(n%i==0)
+		{
+			count++;
+		}
+    }
+    if(count==1)
+    cout<<"Prime";
+    else{
+    	cout<<"Not prime";
+	}
+} 
