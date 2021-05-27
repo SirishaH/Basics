b96e0219eb4df666f4ba6aceccd88ee8d0125c57
SirishaH <84061786+SirishaH@users.noreply.github.com>
2021-05-27 12:25:15 +0530

    Add files via upload

--- Prime(All methods).cpp
+++ Prime(All methods).cpp
@@ -60,4 +60,32 @@ int main()
     else{
     	cout<<"Not prime";
 	}
+}
+
+//PRIME NUMBER METHOD----4
+ #include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,flage=0;
+	cout<<"Enter number";
+	cin>>n;
+	for(int i=2;i<=n-1;i++) //i<=97/2 ; i<=48
+	{
+		if(n%i==0)
+		{                  // n=97
+		flage=1;
+		break;
+		}           
+	} 
+	if(flage==1)
+	       {
+		     cout<<"Not Prime";
+     	   }             
+			else if(n==1)
+			{
+				cout<<"Not prime";
+			  }  
+			  else
+			  cout<<"prime";
 } 
